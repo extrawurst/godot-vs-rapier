e82b9f6cf89ecb4555ebb8a086139e7d7c7fe058
Stephan Dilly <dilly.stephan@gmail.com>
2021-04-14 19:47:54 +0200

    physics state abstraction

--- logic/src/rapier_world.rs
+++ logic/src/rapier_world.rs
@@ -11,18 +11,76 @@ use rapier2d::{
 };
 use std::cell::RefCell;
 
+struct PhysicsState {
+    pub pipeline: PhysicsPipeline,
+    pub broad_phase: BroadPhase,
+    pub narrow_phase: NarrowPhase,
+    pub bodies: RigidBodySet,
+    pub colliders: ColliderSet,
+    pub joints: JointSet,
+    pub ccd: CCDSolver,
+}
+
+impl PhysicsState {
+    fn new() -> Self {
+        Self {
+            pipeline: PhysicsPipeline::new(),
+            broad_phase: BroadPhase::new(),
+            narrow_phase: NarrowPhase::new(),
+            bodies: RigidBodySet::new(),
+            colliders: ColliderSet::new(),
+            joints: JointSet::new(),
+            ccd: CCDSolver::new(),
+        }
+    }
+
+    fn tick(&mut self, gravity: Vector2) {
+        let gravity = na::Vector2::new(gravity.x, gravity.y);
+        let integration_parameters = IntegrationParameters::default();
+
+        self.pipeline.step(
+            &gravity,
+            &integration_parameters,
+            &mut self.broad_phase,
+            &mut self.narrow_phase,
+            &mut self.bodies,
+            &mut self.colliders,
+            &mut self.joints,
+            &mut self.ccd,
+            &(),
+            &(),
+        );
+    }
+
+    fn add_static(&mut self, x: f32, y: f32, w: f32, h: f32) {
+        let floor = RigidBodyBuilder::new_static().translation(x, y).build();
+
+        let floor = self.bodies.insert(floor);
+
+        let floor_collider = ColliderBuilder::cuboid(w, h).build();
+
+        self.colliders
+            .insert(floor_collider, floor, &mut self.bodies);
+    }
+
+    fn add_box(&mut self, x: f32, y: f32) -> RigidBodyHandle {
+        let falling_box = RigidBodyBuilder::new_dynamic().translation(x, y).build();
+        let falling_box = self.bodies.insert(falling_box);
+
+        let box_collider = ColliderBuilder::cuboid(48. * 0.4, 48. * 0.4).build();
+        self.colliders
+            .insert(box_collider, falling_box, &mut self.bodies);
+
+        falling_box
+    }
+}
+
 #[derive(NativeClass)]
 #[inherit(Node2D)]
 pub struct RapierWorld2D {
     #[property]
     gravity: Vector2,
-    pipeline: RefCell<PhysicsPipeline>,
-    broad_phase: RefCell<BroadPhase>,
-    narrow_phase: RefCell<NarrowPhase>,
-    bodies: RefCell<RigidBodySet>,
-    colliders: RefCell<ColliderSet>,
-    joints: RefCell<JointSet>,
-    ccd: RefCell<CCDSolver>,
+    physics: RefCell<PhysicsState>,
     boxes: RefCell<Vec<(RigidBodyHandle, Ref<Node2D>)>>,
 }
 
@@ -33,13 +91,7 @@ impl RapierWorld2D {
 
         Self {
             gravity: Vector2::new(0., 98.),
-            pipeline: RefCell::new(PhysicsPipeline::new()),
-            broad_phase: RefCell::new(BroadPhase::new()),
-            narrow_phase: RefCell::new(NarrowPhase::new()),
-            bodies: RefCell::new(RigidBodySet::new()),
-            colliders: RefCell::new(ColliderSet::new()),
-            joints: RefCell::new(JointSet::new()),
-            ccd: RefCell::new(CCDSolver::new()),
+            physics: RefCell::new(PhysicsState::new()),
             boxes: RefCell::new(Vec::new()),
         }
     }
@@ -51,9 +103,10 @@ impl RapierWorld2D {
 
         godot_print!("size: {},{}", w, h);
 
-        self.add_static(0., h, w, 10.);
-        self.add_static(0., h / 2., 10., h);
-        self.add_static(w, h / 2., 10., h);
+        let mut physics = self.physics.borrow_mut();
+        physics.add_static(0., h, w, 10.);
+        physics.add_static(0., h / 2., 10., h);
+        physics.add_static(w, h / 2., 10., h);
     }
 
     #[export]
@@ -82,41 +135,13 @@ impl RapierWorld2D {
             label.set_text(format!("boxes: {}", count));
         }
 
-        self.tick_rapier();
+        self.physics.borrow_mut().tick(self.gravity);
 
         self.update_boxes(owner);
     }
 
-    fn tick_rapier(&self) {
-        let mut pipeline = self.pipeline.borrow_mut();
-        let mut broad_phase = self.broad_phase.borrow_mut();
-        let mut narrow_phase = self.narrow_phase.borrow_mut();
-        let mut bodies = self.bodies.borrow_mut();
-        let mut colliders = self.colliders.borrow_mut();
-        let mut joints = self.joints.borrow_mut();
-        let mut ccd = self.ccd.borrow_mut();
-
-        let gravity = na::Vector2::new(self.gravity.x, self.gravity.y);
-        let mut integration_parameters = IntegrationParameters::default();
-        integration_parameters.max_position_iterations = 1;
-        integration_parameters.max_linear_correction = 1.;
-
-        pipeline.step(
-            &gravity,
-            &integration_parameters,
-            &mut broad_phase,
-            &mut narrow_phase,
-            &mut bodies,
-            &mut colliders,
-            &mut joints,
-            &mut ccd,
-            &(),
-            &(),
-        );
-    }
-
     fn update_boxes(&self, _owner: &Node2D) {
-        let bodies = self.bodies.borrow_mut();
+        let bodies = &self.physics.borrow().bodies;
 
         for b in self.boxes.borrow().iter() {
             let handle = b.0;
@@ -132,17 +157,11 @@ impl RapierWorld2D {
     }
 
     fn spawn(&self, owner: &Node2D, x: f32, y: f32) {
-        let mut bodies = self.bodies.borrow_mut();
-        let mut colliders = self.colliders.borrow_mut();
+        let mut physics = self.physics.borrow_mut();
+        let handle = physics.add_box(x, y);
+
         let mut boxes = self.boxes.borrow_mut();
-
-        let falling_box = RigidBodyBuilder::new_dynamic().translation(x, y).build();
-
-        let falling_box = bodies.insert(falling_box);
         let falling_box_index = boxes.len();
-
-        let box_collider = ColliderBuilder::cuboid(48. * 0.4, 48. * 0.4).build();
-        colliders.insert(box_collider, falling_box, &mut bodies);
 
         {
             let box_asset = load_scene("res://scenes/RapierBox.tscn").unwrap();
@@ -155,21 +174,8 @@ impl RapierWorld2D {
             let shared_node = new_node.into_shared();
             owner.add_child(shared_node, false);
 
-            boxes.push((falling_box, shared_node));
+            boxes.push((handle, shared_node));
         }
-    }
-
-    fn add_static(&self, x: f32, y: f32, w: f32, h: f32) {
-        let floor = RigidBodyBuilder::new_static().translation(x, y).build();
-
-        let mut bodies = self.bodies.borrow_mut();
-        let floor = bodies.insert(floor);
-
-        let floor_collider = ColliderBuilder::cuboid(w, h).build();
-
-        let mut colliders = self.colliders.borrow_mut();
-
-        colliders.insert(floor_collider, floor, &mut bodies);
     }
 }
 
